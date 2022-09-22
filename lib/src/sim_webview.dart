42ec39c17916e68419852a764b328b9792589f58
IT Apexgroup <it.apexgroup@gmail.com>
2022-09-22 14:40:44 +0630

    second commit

--- lib/src/sim_webview.dart
+++ lib/src/sim_webview.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_webview_pro/webview_flutter.dart';
 import 'package:wakelock/wakelock.dart';
 
